bd2e0d0d00c9238cdda2edd8cd03cdde99d0150e
okovenko <kovolexiy@gmail.com>
2021-11-24 19:34:51 +0200

    Created the default ASP.NET Core 5.0 MVC solution.

---
+++ OAuthTutorial/OAuthTutorial/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OAuthTutorial.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
