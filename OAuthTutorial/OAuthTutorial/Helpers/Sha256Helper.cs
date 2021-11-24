29eb70e69137e52fd316315c918f0ebcff753b66
okovenko <kovolexiy@gmail.com>
2021-11-24 19:46:23 +0200

    Implemented `Sha256Helper` for computing sha256 hashes.

---
+++ OAuthTutorial/OAuthTutorial/Helpers/Sha256Helper.cs
@@ -0,0 +1,18 @@
+﻿using IdentityModel;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace OAuthTutorial.Helpers
+{
+    public static class Sha256Helper
+    {
+        public static string ComputeHash(string codeVerifier)
+        {
+            using var sha256 = SHA256.Create();
+            var challengeBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(codeVerifier));
+            var codeChallenge = Base64Url.Encode(challengeBytes);
+            return codeChallenge;
+        }
+    }
+}
