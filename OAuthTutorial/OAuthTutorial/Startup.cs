d0ab9bb063325e5d4e0d4a657ee7eb91be16f687
Oleksii Kovenko <kovolexiy@gmail.com>
2023-10-15 19:41:11 +0300

    Added session dependencies.

--- OAuthTutorial/OAuthTutorial/Startup.cs
+++ OAuthTutorial/OAuthTutorial/Startup.cs
@@ -24,6 +24,7 @@ namespace OAuthTutorial
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +46,7 @@ namespace OAuthTutorial
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseSession(); 
 
             app.UseEndpoints(endpoints =>
             {
